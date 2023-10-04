19e0c65cce14139aa2cfd4fdafd05f263c624529
terence <terenceshawn18@outlook.com>
2023-10-04 11:45:41 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName (){
+    customerName = customerName.toUpperCase();
+    
+    
+}
+
+function setBestCustomer (){
+    
+    bestCustomer = 'not bob' ;
+    
+}
+function overwriteBestCustomer (){
+    bestCustomer = 'maybe bob'
+}
+
+const leastfavoritecustomer = 'dylan'
+
+function changeLeastFavoriteCustomer(){
+ const leastfavoritecustomer2 = leastfavoritecustomer;
+ leastfavoritecustomer2 = "bobby"
+}
+
+
